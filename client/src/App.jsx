4d8f7ad99db07df0bb523ebad93b705172e411a2
Dagim <dagimgetaW27@gmail.com>
2025-12-10 13:41:47 +0300

    feat: create route for  base layout and other

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserLayout from "./layout/UserLayout";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
+
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center font-text">
-      Help Ethiopia
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<UserLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
