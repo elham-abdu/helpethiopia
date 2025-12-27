a626496c941f63d14e4b31da2d4e16112dc9b8f8
elham-abdu <koniabdu81@gmail.com>
2025-12-28 01:59:24 +0300

    Merged helpethiopia2 into main

--- client/src/feature/home/components/Home.jsx
+++ client/src/feature/home/components/Home.jsx
@@ -1,21 +1,8 @@
-import HomeHero from "./HomeHero";
-import Mission from "./Mission";
-import Impacts from "./Impacts";
-import News from "./News";
-import UrgentCause from "./UrgentCause";
-import WaysToHelp from "./WaysToHelp";
-import CTA from "./CTA";
 
 const Home = () => {
   return (
     <>
-      <HomeHero />
-      <Mission />
-      <Impacts />
-      <News />
-      <UrgentCause />
-      <WaysToHelp />
-      <CTA/>
+   
     </>
   );
 };
